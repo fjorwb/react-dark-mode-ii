dc82c691020150c647703dad17392414496cea06
FJORWB <88600252+fjorwb@users.noreply.github.com>
2024-05-01 18:53:12 -0500

    refactor background & color values

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
     <>
       <DarkMode />
       <h1>React Dark Mode II</h1>
+      <h2>This is a setup for change App Theme</h2>
+      <p>Click on the button in top of page to change the theme</p>
+      <a href="https://www.youtube.com/watch?v=Uz35Qiia84g&t=79s" target='_blank'>source</a>
     </>
   )
 }
